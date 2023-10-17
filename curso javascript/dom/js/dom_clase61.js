56ad5382d53734c2d6a71bc0b5b84f6b000c3cf2
yosepyusen <yaguirreobregon@gmail.com>
2023-10-17 11:07:57 -0500

    clase 61 empezando

---
+++ curso javascript/dom/js/dom_clase61.js
@@ -0,0 +1,5 @@
+console.log("******************** Elementos del documento ********************");
+console.log(window.document); //con esto mostramos todo el codigo html
+console.log(document);//tbm se puede imprimir solo document y ya no window
+console.log(document.head);//imprime: todo lo que hay dentro cabeza:head de doc html 
+console.log(document.body);//imprime: todo lo que hay dentro del body del html
