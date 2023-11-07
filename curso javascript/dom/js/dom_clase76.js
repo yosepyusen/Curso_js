53998e04aeec1fd3746c2fe80329eadfd9f00710
yosepyusen <yaguirreobregon@gmail.com>
2023-11-07 14:52:45 -0500

    clase 76 terminado

--- curso javascript/dom/js/dom_clase76.js
+++ curso javascript/dom/js/dom_clase76.js
@@ -1,32 +1,27 @@
-//delegacion de eventos optimiza mucho al nivel de recuros de meoria
+//delegacion de eventos optimiza mucho al nivel de recuros de meoria, y es usado mas en pticiones asincronas... caso ajax o fetch
 //usando este foreach no es lo mas optimo a nivel de memoria cuando son varios elementos como 1000, usar el addEventListener
 
-const $divsEventos = document.querySelectorAll(".eventos-flujo div"),//seleccionamos la clase:.eventos-flujo, del html y dentro de ello todo los divs que hay
-$linkEventos = document.querySelector(".eventos-flujo a");//seleccionamos la clase:.eventos-flujo, del html y dentro de ello la etiqueta a
+//en estos dos lineas de codigos siguientes ya no es necesario, por eso lo comentamos
+//const $divsEventos = document.querySelectorAll(".eventos-flujo div"),//seleccionamos la clase:.eventos-flujo, del html y dentro de ello todo los divs que hay
+//$linkEventos = document.querySelector(".eventos-flujo a");//seleccionamos la clase:.eventos-flujo, del html y dentro de ello la etiqueta a
 
 function flujoEventos(e){
-    console.log(`Hola te saluda "${this.className}", el click lo origino "${e.target.className}"`);
-   //e.stopPropagation();//con esto evitamos que se imprime tres o dos veces la frase si diste click en div 3 o div 2 respectivamente, funciona en modo de BURBUJA o MODO CAPTURA
+    console.log(`Hola te saluda "${this}", el click lo origino "${e.target.className}"`);//${this.className(sale undefined)}, antes en clase 75 se guardaban en una variable y si tenia className, en este caso el document el documento no tiene un atributo className
+    //${this}, si dejamos asi sale [object Window], todo lo que cuelga de widow no es necesario ponerle window
+    //e.stopPropagation();//ya no es necesario usar stopPropagation, ya no se hace propagacion porque el evento ha sido asignado al elemento padre, que e este caso es el document
 }
 
-document.addEventListener("click", (e)=>{
+document.addEventListener("click", (e)=>{//con esto seleccionamos un solo evento o una sola asignacion al evento click y mejoramos rendimiento de memoria y recursos y con condicional capturamos el evento
     console.log("Click en", e.target);//con esto el evento click esta asignado al document, ya que imprime a la etiqueta donde das click, ejem: Click en <h3>​Flujo de eventos​</h3>​
+    
+    if(e.target.matches(".eventos-flujo div")){//e.target:es decir el obj. que origino el evento, busca con matches el selector valido que es div
+        flujoEventos(event);//ejecutamos la funcion flujoEventos y le pasamos el evento "e"
+    }
+
     //target:button, elemento u obj. que origino el evento
-    if(e.target.matches(".eventos-flujo a")){//e.target:es decir el obj. que origino el evento, busca con matches el selector valido que es
+    if(e.target.matches(".eventos-flujo a")){//e.target:es decir el obj. que origino el evento, busca con matches el selector valido que es la etiqueta "a"
         alert("Hola soy tu amigo y docente..Digital Jonathan mircha");
         e.preventDefault();//cancelar la accion que tenga por defecto el elemento en este caso es la etiqueta a, es decir que al dar click no nos lleva al enlace sino cancela
         //e.stopPropagation();//ya no es necesario poner stopPropagation
     }
 });
-
-// $divsEventos.forEach((div)=>{
-//     div.addEventListener("click",flujoEventos);//metodo burbuja de mas interno a externo elemento, 
-// });
-
-// const $linkEventos = document.querySelector(".eventos-flujo a"); //seleccionamos la clase:.eventos-flujo, del html y dentro de ello la etiqueta a
-
-//     $linkEventos.addEventListener("click",e=>{//primer parametro es el click y segundo es el arrow function del addEventListener
-//         alert("Hola soy tu amigo y docente..Digital Jonathan mircha");
-//         e.preventDefault();//cancelar la accion que tenga por defecto el elemento en este caso es la etiqueta a, es decir que al dar click no nos lleva al enlace sino cancela
-//         e.stopPropagation();//con esto ya no imprime la frase de la linea 5 en consola 
-//     });
