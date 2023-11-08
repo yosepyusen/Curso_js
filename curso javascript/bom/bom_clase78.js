0446899ed2a6fcf1399d9d790abb855c401973f1
yosepyusen <yaguirreobregon@gmail.com>
2023-11-08 10:04:16 -0500

    class 78

---
+++ curso javascript/bom/bom_clase78.js
@@ -0,0 +1,6 @@
+// window.alert("Alerta");
+// window.confirm("Confirmacion");//con esto validamos a true(aceptar) o false(cancelar)
+//window.prompt("Aviso");//es un mensaje o aviso de texto, que tiene boton a true(aceptar) o false(cancelar), y adicional agrega un input(para poder escribir algo)
+
+//todo lo que cuelga de window no es necesario poner window punto alert, confirm,promt, sino directamente el alert, confirm,promt...
+//alert("jon");
