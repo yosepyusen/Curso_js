879e3e77d670c42204083b06bf12041356461b8f
yosepyusen <yaguirreobregon@gmail.com>
2023-11-08 11:03:54 -0500

    terminando clase 78

--- curso javascript/bom/bom_clase78.js
+++ curso javascript/bom/bom_clase78.js
@@ -4,3 +4,23 @@
 
 //todo lo que cuelga de window no es necesario poner window punto alert, confirm,promt, sino directamente el alert, confirm,promt...
 //alert("jon");
+
+const $btnAbrir = document.getElementById("abrir-ventana"),//seleccionamos el id="abrir-ventana" y para los dos botones sig. del html 
+$btnCerrar = document.getElementById("cerrar-ventana"),
+$btnPrint = document.getElementById("imprimir-ventana");
+
+let ventana;//variable ventana sin valor
+
+$btnAbrir.addEventListener("click",e=>{
+    ventana=window.open("https://jonmircha.com");//para abrir una ventana es window open y almacenamos en variable ventana
+    //open("https://jonmircha.com");//pero tabien podemos poner solo open
+});
+
+$btnCerrar.addEventListener("click",(e)=>{
+    //window.close();//Los scripts pueden cerrar solo las ventanas que abrieron.
+    ventana.close();//para cerrar solo las ventanas que abrieron por el metodo open, en este caso cierra la ventana abierto con metodo open: https://jonmircha.com
+});
+
+$btnPrint.addEventListener("click",e=>{
+    window.print();
+});
