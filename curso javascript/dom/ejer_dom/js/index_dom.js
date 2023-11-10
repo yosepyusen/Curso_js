7db888531b887a5eb3c34c26b870746b527cec58
yosepyusen <yaguirreobregon@gmail.com>
2023-11-10 17:52:56 -0500

    btn iteractivo

--- curso javascript/dom/ejer_dom/js/index_dom.js
+++ curso javascript/dom/ejer_dom/js/index_dom.js
@@ -1,3 +1,8 @@
-import hamburgerMenu from "./menu_hamburguesa";//podemos escribir solo "hambu" y dar tab y se nos autocompleta este codigo
+import hamburgerMenu from "./menu_hamburguesa.js";//podemos escribir solo "hambu" y dar tab y se nos autocompleta este codigo
 
-hamburgerMenu(".panel-btn",".panel");//seleccionamos las dos clase del html
+const d=document;
+
+d.addEventListener("DOMContentLoaded",(e)=>{ //cuando carga el documento, ya que DOMContentLoad:es para que cargue la pagina es mejor usar esto
+    hamburgerMenu(".panel-btn",".panel");//caudo ya eta cargado la pagina entonces ejecutamos el evento hamburgerMenu, seleccionamos las dos clase del html que es:".panel-btn",".panel"
+    
+});
--- curso javascript/dom/ejer_dom/js/menu_hamburguesa.js
+++ curso javascript/dom/ejer_dom/js/menu_hamburguesa.js
@@ -1,7 +1,11 @@
 
 export default function hamburgerMenu(panelBtn,panel){
     const d= document; //guardamos en variable "d", el document 
-    d.addEventListener("click",e=>{
-        if(e.target.matches(panelBtn)); //si el obj.(target) que origina el evento, coincide con el selector que me estas dando en el panelBtn
+
+    d.addEventListener("click",(e)=>{//si doy click en el boton de clase(.panel-btn) que le pasamos en index_dom.js, luego ejecutamos el if()
+        
+        if(e.target.matches(panelBtn)){  //si el obj.(target) que origina el evento, coincide con el selector que me estas dando en el panelBtn
+            d.querySelector(panel).classList.toggle("is-active");//con esto le decimos que selecionamos la clase(.panel guardado en varible panel), con classList para entrar en su lista de clase y utiliza el metodo toogle y intercambiar esa clase "is-active"
+        }
     })
 }
