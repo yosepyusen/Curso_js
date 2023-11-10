502c58b8ad04de4f0f2737477146a715396c0f31
yosepyusen <yaguirreobregon@gmail.com>
2023-11-10 17:06:35 -0500

    funcionalidad de btn con js

---
+++ curso javascript/dom/ejer_dom/js/index_dom.js
@@ -0,0 +1,3 @@
+import hamburgerMenu from "./menu_hamburguesa";//podemos escribir solo "hambu" y dar tab y se nos autocompleta este codigo
+
+hamburgerMenu(".panel-btn",".panel");//seleccionamos las dos clase del html
--- curso javascript/dom/ejer_dom/js/menu_hamburguesa.js
+++ curso javascript/dom/ejer_dom/js/menu_hamburguesa.js
@@ -1,1 +1,7 @@
-//
+
+export default function hamburgerMenu(panelBtn,panel){
+    const d= document; //guardamos en variable "d", el document 
+    d.addEventListener("click",e=>{
+        if(e.target.matches(panelBtn)); //si el obj.(target) que origina el evento, coincide con el selector que me estas dando en el panelBtn
+    })
+}
