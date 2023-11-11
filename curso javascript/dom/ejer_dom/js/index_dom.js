1fa9b1c606c2a44c693f7fbdfe575c435bb68707
yosepyusen <yaguirreobregon@gmail.com>
2023-11-11 18:59:04 -0500

    clase en reloj digital

--- curso javascript/dom/ejer_dom/js/index_dom.js
+++ curso javascript/dom/ejer_dom/js/index_dom.js
@@ -1,7 +1,10 @@
 import hamburgerMenu from "./menu_hamburguesa.js";//podemos escribir solo "hambu" y dar tab y se nos autocompleta este codigo
+import { digitalClock,alarm } from "./reloj.js"; //como no hay ningun funcion por default uso la destructuracion de obj.
 
 const d=document;
 
 d.addEventListener("DOMContentLoaded",(e)=>{ //cuando carga el documento, ya que DOMContentLoad:es para que cargue la pagina es mejor usar esto
     hamburgerMenu(".panel-btn",".panel",".menu a",".hamburger-inner",".hamburger-box");//caudo ya eta cargado la pagina entonces ejecutamos el evento hamburgerMenu, seleccionamos las dos clase del html que es:".panel-btn",".panel"
+    digitalClock("#reloj","#activar-reloj","#desactivar-reloj"); //como es id se pone delante un #, y se selecciona las tres id del button
 });
+
---
+++ curso javascript/dom/ejer_dom/js/reloj.js
@@ -0,0 +1,19 @@
+//----- CLASE 80 -----------------
+const d = document;
+
+export function digitalClock(clock,btnPlay,btnStop){
+    d.addEventListener("click",e=>{
+        if(e.target.matches(btnPlay)){//cuando el obj. origina el evento y coincide con el evento btnPlay
+            setInterval(() => { //que se ejecuta cada segundo(1000) o se actualice la hora por eso utilizamos setInterval
+                let clockHour = new Date().toLocaleTimeString();//con esto vemos la hora y guardamos en clockHour
+                d.querySelector(clock).innerHTML = `<h3>${clockHour}</h3>`;//seleccionamos #reloj, y en su contenido innerHTML decimos que interpole el valor de clockHour cada segundo
+            }, 1000);//
+            e.target.disabled=true;//desabilitamos el boton una vez dado clic en id="#reloj"
+        }
+        if(e.target.matches(btnStop)){//cuando el obj. origina el evento y coincide con el evento btnStop
+
+        }
+    });
+}
+
+export function alarm(){}
