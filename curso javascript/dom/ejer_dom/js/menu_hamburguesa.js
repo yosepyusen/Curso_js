48fbfb06766a1dd12611fa0e674d3b4f29c7eb34
yosepyusen <yaguirreobregon@gmail.com>
2023-11-10 23:12:40 -0500

    corrijiendo btns

--- curso javascript/dom/ejer_dom/js/menu_hamburguesa.js
+++ curso javascript/dom/ejer_dom/js/menu_hamburguesa.js
@@ -1,10 +1,10 @@
 
-export default function hamburgerMenu(panelBtn,panel){
+export default function hamburgerMenu(panelBtn,panel,hamburgerInner,hamburgerBox){
     const d= document; //guardamos en variable "d", el document 
 
     d.addEventListener("click",(e)=>{//si doy click en el boton de clase(.panel-btn) que le pasamos en index_dom.js, luego ejecutamos el if()
         
-        if(e.target.matches(panelBtn)){  //si el obj.(target) que origina el evento, coincide con el selector que me estas dando en el panelBtn
+        if(e.target.matches(panelBtn) || e.target.matches(hamburgerInner) || e.target.matches(hamburgerBox)){  //si el obj.(target) que origina el evento, coincide con el selector que me estas dando en el panelBtn
             d.querySelector(panel).classList.toggle("is-active");//con esto le decimos que selecionamos la clase(.panel guardado en varible panel), con classList para entrar en su lista de clase y utiliza el metodo toogle y intercambiar esa clase "is-active"
         }
     })
