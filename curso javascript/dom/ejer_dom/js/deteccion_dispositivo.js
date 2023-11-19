899fa90ac682a28ceb9b45af30d9b6406dbda431
yosepyusen <yaguirreobregon@gmail.com>
2023-11-19 15:48:09 -0500

    26 min de class 92

--- curso javascript/dom/ejer_dom/js/deteccion_dispositivo.js
+++ curso javascript/dom/ejer_dom/js/deteccion_dispositivo.js
@@ -8,5 +8,23 @@ const d = document,
     n = navigator,
     ua = n.userAgent
 export default function userDeviceInfo(id){//id donde captura toda la informacion que se va extraer del user-agent
-    console.log(ua);//imprime: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36
-}
+    
+    const $id = d.getElementById(id),
+
+    isMobile = { //isMobile: si viene de un dispositivo, isMobile, tiene propiedades 
+        //la variable: android, ios y window esta declarado en una arrow function
+        //El método match() devuelve todas las ocurrencias de una expresión regular dentro de una cadena.
+        android: () => ua.match(/android/i), //ua.match(/android/), user agent o que va tratar de buscar si en la cadena del texto del user agent, encontro la palabra "android", y la bandera: i(ignora mayusc y minculas), se esta usando expresion regular: /android/
+        ios: ()=> ua.match(/iphone|ipad|ipod/i),//detectar si tiene la palabra iphone,ipad,ipod el user agent
+        windows: ()=> ua.match(/windows phone/i),//detectar si tiene la palabra windows phone, para dispositivos de windows
+        any: function(){ //esta declara en una funcion anonima, porque hace referencia a los 3 propiedades de este obj. por eso uso f.anonima y si ponemos arrow function nos va dar el this del contexto global
+            return this.android() || this.ios() || this.windows();
+        },
+    }, 
+    isDesktop = {},//isDesktop: si viene de un pc
+    isBrowser = {};
+
+    console.log(isMobile.ios());
+    //console.log(ua);//imprime: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 , nos buen info. del dispositivo desde que nos visita el usuario, la info. va cambiar segun el navegador en que estas y el pc o mobil  
+    //en inspeccionar si cambiamos a mobil nos imprime: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1 , en este caso dice que estmos en moobil
+}   
