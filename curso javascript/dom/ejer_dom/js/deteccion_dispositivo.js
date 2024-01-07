44cd8b212275b71839255a09f0d0e24514e4ef99
yosepyusen <yaguirreobregon@gmail.com>
2024-01-07 18:01:27 -0500

    92 trminando

--- curso javascript/dom/ejer_dom/js/deteccion_dispositivo.js
+++ curso javascript/dom/ejer_dom/js/deteccion_dispositivo.js
@@ -7,6 +7,7 @@
 const d = document,
     n = navigator,
     ua = n.userAgent
+    //d,n, ua : son variables donde se guardan forma abreviada document, navigator, useragent 
 export default function userDeviceInfo(id){//id donde captura toda la informacion que se va extraer del user-agent
     
     const $id = d.getElementById(id),
@@ -21,10 +22,44 @@ export default function userDeviceInfo(id){//id donde captura toda la informacio
             return this.android() || this.ios() || this.windows();
         },
     }, 
-    isDesktop = {},//isDesktop: si viene de un pc
-    isBrowser = {};
+    
+    //isDesktop: si viene de un pc
+    isDesktop = {
+        linux: () => ua.match(/linux/i), 
+        mac: ()=> ua.match(/mac os/i),
+        windows: ()=> ua.match(/windows nt/i),
+        any: function(){ 
+            return this.linux() || this.mac() || this.windows();//
+        }   
+    },
+    isBrowser = {
+        chrome: () => ua.match(/chrome/i), 
+        safari: ()=> ua.match(/safari/i),
+        firefox: ()=> ua.match(/firefox/i),
+        opera: ()=> ua.match(/opera|opera mini/i),
+        ie: ()=> ua.match(/msie|iemobile/i),
+        edge: ()=> ua.match(/edge/i),
+        any: function(){ //
+            return (
 
-    console.log(isMobile.ios());
+                this.chrome() ||
+                this.safari() ||
+                this.firefox() ||
+                this.opera() ||
+                this.ie() ||
+                this.edge()
+                
+            );
+        }, 
+    };
+    console.log(isMobile.android());//si sale null en consola es porque no hay ese tipo de celular o pc, es mas para validar
     //console.log(ua);//imprime: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 , nos buen info. del dispositivo desde que nos visita el usuario, la info. va cambiar segun el navegador en que estas y el pc o mobil  
     //en inspeccionar si cambiamos a mobil nos imprime: Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1 , en este caso dice que estmos en moobil
+
+    $id.innerHTML = `
+    <ul>
+        <li>User Agent: <b>${ua}</b></li>
+        <li>Plataforma: <b>${ua}</b></li>
+    </ul>
+    `;
 }   
