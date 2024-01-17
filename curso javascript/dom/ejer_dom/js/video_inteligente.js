7f307865c79a2ebda00aa84238264b69af862aca
yosepyusen <yaguirreobregon@gmail.com>
2024-01-17 14:27:16 -0500

    101 terminndo

--- curso javascript/dom/ejer_dom/js/video_inteligente.js
+++ curso javascript/dom/ejer_dom/js/video_inteligente.js
@@ -10,10 +10,10 @@ export default function smartVideo(){
         //console.log("entries", entrada); //imprime:(2) [IntersectionObserverEntry, IntersectionObserverEntry]
 
         entrada.forEach(entry => { //por cada entrada ejecuta lo sig.
-            
+
             //la sig. linea de cod. es para ver que atributos tiene el entry
             //console.log("Entry", entry);//imprime dos veces el obj. Entry pero solo pongo uno a modo de lectura: Entry IntersectionObserverEntry {time: 289.0999999642372, rootBounds: DOMRectReadOnly, boundingClientRect: DOMRectReadOnly, intersectionRect: DOMRectReadOnly, isIntersecting: false, …}
-            
+
             if(entry.isIntersecting){//si ya ha cruzado la intersection
                 //dentro de atributos deñ entry esta el target y dentro de ello esta el video y play es para reproduzca, funcion de js algo asi
                 entry.target.play();//si ya tenemos la interseccion, simplemente accedo al metodo play para reproducir
@@ -21,14 +21,33 @@ export default function smartVideo(){
                 //caso contrario paramos el video
                 entry.target.pause();
             }
-            //ponemos dentro del foreach, para poder ejecutar el visibilitychange en todo los videos
-            w.addEventListener("visibilitychange", e=> d.visibilityState === "visible"   //propiedad del document:visibilitychange
-                ? entry.target.play() //d.visibilityState === "visible", si esto es igual entonces ejecuta el play
-                : entry.target.pause()//caso contrario, q pausa el video
-            );
+
+            //ponemos dentro del foreach, para poder ejecutar el visibilitychange en todo los videos 
+            //LE COMENTE PORQUE CUANDO PRESIONAS ALT MAS TAB SE PONE A REPRODUCIR  
+            // w.addEventListener("visibilitychange", e=> d.visibilityState === "visible"   //propiedad del document:visibilitychange
+            //     ? entry.target.play() //d.visibilityState === "visible", si esto es igual entonces ejecuta el play
+            //     : entry.target.pause()//caso contrario, q pausa el video
+            // );
         });
     }
     const observer =  new IntersectionObserver(cb, { threshold:0.5}); //cuando el elemento se vean a 50%, ahi haga la observacion y lo reproduzca
 
     $video.forEach(elem=>observer.observe(elem)); //por cada elemento ejecuta la funcion observer y el metodo observer, en este caso el elemento a observar es la lista de nodos de los videos
+
+
+    //falta completar
+    //const video = d.querySelector("video");
+    // d.addEventListener("keydown", e=>{
+    //     //if(e.key === e.TabKey && e.altKey  ){
+    //     if(e.keyCode === 18 && 9 ){
+    //         // console.log("hola");
+    //         // console.log($video);
+    //         //entry.target.pause();
+    //     }
+    //     //console.log(e.keyCode)
+    //     if(e.key === "k" && e.altKey){
+    //        //entry.target.pause();
+    //     }
+    // });
+
 }
