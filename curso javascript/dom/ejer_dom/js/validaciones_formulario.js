3cbb3ae3071ef8e7c46560709f133d2d43aa617a
yosepyusen <yaguirreobregon@gmail.com>
2024-01-22 15:32:20 -0500

    validaciones medio 102

--- curso javascript/dom/ejer_dom/js/validaciones_formulario.js
+++ curso javascript/dom/ejer_dom/js/validaciones_formulario.js
@@ -9,7 +9,7 @@ export default function contacFormValidation(){
 
     $inputs.forEach(input=>{//por cada input 
         const $span = d.createElement("span"); //creamos una etiqueta span
-        $span.id = input.name; //vamos asignarle como atributo id el valor que viene en nuestro input del html en su propiedad name para que sea unico
+        $span.id = input.name; //creamos atributo id de span y el valor de input del html de su propiedad name le asignamos
         //ejemplo:en 1mer input en su propiedad name="name", el name se va agregar al id del span
         
         //despues agregamos una lista de clases, previamente creada en el css: .contact-form-error"
@@ -19,13 +19,40 @@ export default function contacFormValidation(){
         $span.textContent = input.title; // a este span ponle en su propiedad textcontend lo ve venga en el atributo title del input:input.title
         input.insertAdjacentElement("afterend",$span); //inserta el espan de manera adjacente en el hrno siguiente:afterend
     });
-    //las validaciones se puede hacer en el momento que pulse el boton submit o al momento que el usuario va escribiendo
 
-    d.addEventListener("keyup", e=>{//keyup:al momento que levante la tecla o suelta
+    //las validaciones se puede hacer en el momento que pulse el boton submit o al momento que el usuario va escribiendo
+    d.addEventListener("keyup", (e)=>{//keyup:al momento que levante la tecla o suelta
        
-        if(e.target.matches(".contact-form [required]")){  //se ejecuta el evento cuando el e.target.matches coincidad un elemento que esta dentro del formulario con el atributo required
-            // let $input = e.target,
-            //  pattern     
+
+
+        if(e.target.matches(".contact_form [required]")){  //se ejecuta el evento cuando el e.target.matches coincidad un elemento que esta dentro del formulario con el atributo required
+            let $input = e.target, //elementos que originan el evento 
+            
+            pattern = $input.pattern || $input.dataset.pattern;//cada input tiene su propio patron, bien valida $input.pattern(para inputs) o $input.dataset.pattern(para textarea)
+            //input.pattern:del input traete el atributo patern
+            //$input.dataset.patern, lista completa de data-atribute de atributos del input esta en dataset y luego el nombre del atributo patern
+            
+            //el input de ASUNTO A TRATAR Y EL TEXTAREA, no tienen patern y comentamos porque se uso a modo de aprendizaje
+            //console.log($input, pattern);// con esto imprime la etiqueta: input con todo sus atributos ejem: <input type="text" ... required>
+            
+            //tenemos 2 formas de validar
+            
+            if(pattern && $input.value !==""){  //primer caso: cuado tienen atributo patern y cuando el valor del input sea diferente de nada(para el text area y los demas inputs)
+                //console.log("El input tiene patrón");//a modo de aprendizaje
+                
+                let regex = new RegExp(pattern); //creamos un regulacion expresion, para validaciones donde interno de pattern tiene un regExp para validar 
+                
+                return !regex.exec($input.value)  //si la expresion en regex, no valida lo que viene de valor del input 
+                //con e.target.value($input.value): sacamos lo que escribe en la pag web dentro del input
+                //exec es metodo para ejecutar y mostrar todo atributos del regex:expresion regular    
+                
+                ? d.getElementById($input.name).classList.add("is-active") //parte verdadera, $input.name:con esto capturamos el id del span q s creo dinamicamente
+                : d.getElementById($input.name).classList.remove("is-active")//parte falsa, aca removemos la clase
+            }
+
+            if(!pattern){ //2do caso cuando no tienen patron o atributo patern
+                console.log("El input NO tiene patrón");
+            }
         }
         }); 
 }
