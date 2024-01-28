7c120dc14e56178f783a28541a4cfb3c3082521e
yosepyusen <yaguirreobregon@gmail.com>
2024-01-28 09:52:33 -0500

    clase medio 106

--- curso javascript/ajax/js/ajax.js
+++ curso javascript/ajax/js/ajax.js
@@ -18,9 +18,28 @@
         //PASO N°2
         //segundo forma 
         xhr.addEventListener("readystatechange", e => {  //readystatechange, se lanza cuando detecta cualquier cambio, cuando hay error, abortado, completado,...
-            console.log(xhr); // imprime 4 veces: XMLHttpRequest {onreadystatechange: null, readyState: 1, timeout: 0, withCredentials: false, upload: XMLHttpRequestUpload, …}
+            
+            //console.log(xhr); // imprime 4 veces: XMLHttpRequest {onreadystatechange: null, readyState: 1, timeout: 0, withCredentials: false, upload: XMLHttpRequestUpload, …}
             //esos 4 veces es porque hay 5 estados la cual el 1mero no cuenta: READY_STATE_UNINITIALIZED, READY_STATE_LOADING, READY_STATE_LOADED, READY_STATE_INTERACTIVE, READY_STATE_COMPLETE
-
+            
+            if(xhr.readyState !== 4) return; //cuando el readyState del xhr sea diferente de 4
+                
+                console.log(xhr);//ahora solo imprime un XMLHttpRequest y en pestaña network debe de salir 200, que es un cod. de ok , 404 es de not found
+                //ademas es un json responseText:"[\n  {\n.. , del XMLHttpRequest 
+                /*
+                Respuestas informativas (100 - 199)
+                Respuestas satisfactorias ( 200 - 299) , este mas nos interesa
+                Mensajes de redireccionamiento ( 300 - 399)
+                Respuestas de error del cliente ( 400 - 499)
+                Respuestas de error del servidor ( 500 - 599)
+                */
+            if(xhr.status >= 200 && xhr.status < 300) { ; //cuando el xhr.status es mayor a 200 y menor a 300
+                
+                console.log("exito"); //saldra cuando la url este bien
+                
+            }else{
+                console.log("error");//cuando el url esta mal
+            }
         }); 
 
         //PASO N°3
