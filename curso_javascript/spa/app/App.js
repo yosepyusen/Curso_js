d327a4cce600d6fed6a11f8a1cd4f8a638f321f2
yosepyusen <yaguirreobregon@gmail.com>
2024-03-09 04:35:59 -0500

    class 146 fin y add coments en modulos.html

--- curso_javascript/spa/app/App.js
+++ curso_javascript/spa/app/App.js
@@ -2,9 +2,13 @@
 //App.js es un componente
 
 //para las pc es mas faciles procesar una funcion q clase por eso usa react
+//ponemos api es nombre q damos, hace referencia a los end points q vamos usar es decir peticiones 
+import api from "./helpers/wp_api.js" //en vanilla js si es poner extension .js pero en react y otros no es necesario
 
 export function App(){
 
     document.getElementById("root").innerHTML = `<h1>Bienvenido a spa con vailla js</h1>`;
-
+    
+    console.log(api); //imprime rutas de api {NAME: 'lucylara', DOMAIN: 'https://lucylara.com', SITE: 'https://lucylara.com/wp-json', API_WP: 'https://lucylara.com/wp-json/wp/v2', POSTS: 'https://lucylara.com/wp-json/wp/v2/posts?_embed', …}
+    
 }
---
+++ curso_javascript/spa/app/helpers/wp_api.js
@@ -0,0 +1,24 @@
+// api rest:https://developer.wordpress.org/rest-api/ 
+// https://developer.wordpress.org/rest-api/reference/ donde estan : Referencia del punto final del desarrollador de API REST
+// https://developer.wordpress.org/rest-api/reference/posts/ : dentro de posts
+
+//tener disponibles mediante SPA el buscador que ya nos proporciona wordpres
+const NAME = "lucylara",
+    DOMAIN = `https://${NAME}.com`,
+    SITE = `${DOMAIN}/wp-json`,
+    API_WP = `${SITE}/wp/v2`,
+    POSTS = `${API_WP}/posts?_embed`, //?_embed para traiga toda la info referente al posts
+    POST = `${API_WP}/posts`, //aqui solo traiga info. de manera sencilla
+    SEARCH = `${API_WP}/search?_embed&search=`; //?_embed:para toda la info completa ; &(y) search= (esto va tomar lo q el usuario introduzca en el input a buscar)
+
+export default{ //esto tambien se puede llamar en App.js con import y si es funcion solo el nombre de la funcion y si es variable declarada entre corchetes ver en mas en file:js_con_html 
+    //NOMBRE:NOMBRE pero si el nombre del parametro de la propiedad obj. es igual al nombre de donde esta guardado la variable entonces podemos poner solo el valor en este caso NOMBRE
+    //es valido de ES6
+    NAME, //ya tienes el valor pero falta el nombre de propiedad obj. 
+    DOMAIN,
+    SITE,
+    API_WP,
+    POSTS,
+    POST,
+    SEARCH
+}
